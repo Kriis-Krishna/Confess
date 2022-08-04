864e7e5a2bfd815e645e8f82dc15935838f0be90
kriis <kriis.krish@gmail.com>
2022-08-04 21:17:46 +0530

    Profile Added

--- app/src/main/java/com/example/confess/Authorize.java
+++ app/src/main/java/com/example/confess/Authorize.java
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Authorize extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_authorize);
 
-        startActivity(new Intent(getApplicationContext(), AddJournal.class));
+//        startActivity(new Intent(getApplicationContext(), Home.class));
     }
 }
